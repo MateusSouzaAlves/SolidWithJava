16364fc5b43a8766cadb0a99202eec76fa5e9abc
solutionmateus <solution.mateus@gmail.com>
2025-07-11 00:17:26 -0300

    SOLID com Harry Potter

--- Solid With Harry Potter/src/O/Certo/Dementador.java
+++ Solid With Harry Potter/src/O/Certo/Dementador.java
@@ -3,7 +3,7 @@ package O.Certo;
 // ✅ Certo: Interface aberta para novas classes, sem alterar a lógica principal.
 // Agora podemos adicionar novas criaturas sem alterar o feitiço! Muito mais mágico e flexível.
 
-public class Dementador {
+public class Dementador implements Criatura{
     public void defender() {
         System.out.println("Expecto Patronum!");
     }
